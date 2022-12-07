fdb57a1df8e33c2d46111e6d87f98e44ca0123fb
K Hareesh <hareesh.komma@improve10x.com>
2022-12-07 18:22:51 +0530

    we add default in messages, series,and Template

--- app/src/main/java/com/improve10x/crud/series/SeriesItem.java
+++ app/src/main/java/com/improve10x/crud/series/SeriesItem.java
@@ -4,10 +4,10 @@ import com.google.gson.annotations.SerializedName;
 
 public class SeriesItem {
     @SerializedName("_id")
-    public String Id;
-    public String seriesId;
+    String Id;
+    String seriesId;
     @SerializedName("title")
-    public String title;
+    String title;
     @SerializedName("imageUrl")
-    public String imageUrl;
+    String imageUrl;
 }
