f87490b2266b4a5642607d73493097b597d21239
K Hareesh <hareesh.komma@improve10x.com>
2022-12-08 12:06:14 +0530

    in logcat on create and on resume called

--- app/src/main/java/com/improve10x/crud/messages/MessagesActivity.java
+++ app/src/main/java/com/improve10x/crud/messages/MessagesActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -27,6 +28,7 @@ public class MessagesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messages);
+        Log.i("MessagesActivity", "onCreate");
         getSupportActionBar().setTitle("Messages");
         handleAdd();
         setupData();
@@ -56,6 +58,7 @@ public class MessagesActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        Log.i("MessagesActivity", "onResume");
         fetchMessages();
     }
 
