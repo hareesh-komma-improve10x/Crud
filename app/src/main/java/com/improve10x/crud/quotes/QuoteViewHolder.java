8fd1a4870059cc0813abf873621b9fc33f1bdcb9
K Hareesh <hareesh.komma@improve10x.com>
2022-12-14 16:20:36 +0530

    implemented the viewHolder class

--- app/src/main/java/com/improve10x/crud/quotes/QuoteViewHolder.java
+++ app/src/main/java/com/improve10x/crud/quotes/QuoteViewHolder.java
@@ -1,15 +1,27 @@
 package com.improve10x.crud.quotes;
 
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.improve10x.crud.R;
+
+import org.w3c.dom.Text;
+
 public class QuoteViewHolder extends RecyclerView.ViewHolder {
 
-
+    ImageView quoteImageImg;
+    TextView quoteTextTxt;
+    TextView authorNameTxt;
 
     public QuoteViewHolder(@NonNull View itemView) {
         super(itemView);
+        quoteImageImg = itemView.findViewById(R.id.quote_image_img);
+        quoteTextTxt = itemView.findViewById(R.id.quote_text_txt);
+        authorNameTxt = itemView.findViewById(R.id.author_name_txt);
     }
 }
