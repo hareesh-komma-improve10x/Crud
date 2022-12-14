da7dab24906d62f39eee60eea667f58d0dc5f598
K Hareesh <hareesh.komma@improve10x.com>
2022-12-14 16:03:56 +0530

    we design quote_item.xml

---
+++ app/src/main/java/com/improve10x/crud/quotes/QuoteViewHolder.java
@@ -0,0 +1,15 @@
+package com.improve10x.crud.quotes;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class QuoteViewHolder extends RecyclerView.ViewHolder {
+
+
+
+    public QuoteViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
