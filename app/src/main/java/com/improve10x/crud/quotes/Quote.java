2f77f466235f75d3f49d3a203aef1911afabdcf3
K Hareesh <hareesh.komma@improve10x.com>
2022-12-15 10:19:36 +0530

    we implementing the xml design correction of back ground color and delete implemented

--- app/src/main/java/com/improve10x/crud/quotes/Quote.java
+++ app/src/main/java/com/improve10x/crud/quotes/Quote.java
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName;
 import java.io.Serializable;
 
 public class Quote implements Serializable {
-    @SerializedName("id")
+    @SerializedName("_id")
     String id;
     @SerializedName("quote")
     String quoteText;
